44dc6428a1ce4b3cb53840fef199844d7d0f2d5c
max-v-walker <maxwalker23@gmail.com>
2021-05-14 11:58:59 -0400

    Updated routes, index.js, and functioning CRUD

--- controllers/postRouter.js
+++ controllers/postRouter.js
@@ -1,45 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/PostModel');
+const Post = require("../models/PostModel");
 
 // Index: GET all the Posts
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   Post.find({})
-    .populate('author')
-    .exec((error, authors) => {
-      if (error) return console.log('author population error')
-    })
+    // .populate('author')
+    // .exec((error, authors) => {
+    //   if (error) return console.log('author population error')
+    // })
     .then((posts) => res.json(posts))
     .catch(next);
 });
 
 // Show: Get a Post by ID
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   Post.findById(req.params.id)
-    .populate('author')
-    .exec((error, author) => {
-      if (error) return console.log('author population error')
-    })
+    // .populate("author")
+    // .exec((error, author) => {
+    //   if (error) return console.log("author population error");
+    // })
     .then((posts) => res.json(posts))
     .catch(next);
 });
 
 // Create: Create a new post
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   Post.create(req.body)
     .then((newPost) => res.json(newPost))
     .catch(next);
 });
 
 // Update: Edit a Post
-router.put('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   Post.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
     .then((post) => res.json(post))
     .catch(next);
 });
 
 // Delete: Delete a Post in the database
-router.delete('/:id', (req, res, next) => {
+router.delete("/:id", (req, res, next) => {
   Post.findOneAndDelete({ _id: req.params.id })
     .then((post) => res.json(post))
     .catch(next);
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const userRouter = require('./controllers/userRouter')
-const postRouter = require('./controllers/postRouter')
-const methodOverride = require('method-override')
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
+const express = require("express");
+const userRouter = require("./controllers/userRouter");
+const postRouter = require("./controllers/postRouter");
+const methodOverride = require("method-override");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 const port = 4000;
 
-
-app.get('/', (req, res)=> {
+app.get("/", (req, res) => {
   // Can also change just a sample size for the route
-  res.send("You're Home")
-})
-
+  res.send("You're Home");
+});
 
 // We can discuss what we want on each route and where it should go
-// app.use('/user/', userRouter)
-// app.use('/post/', postRouter)
+app.use("/users/", userRouter);
+app.use("/posts/", postRouter);
 
-app.listen(port, ()=>{
-  console.log(`Project 3 HOSTED on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Project 3 HOSTED on port ${port}`);
+});
