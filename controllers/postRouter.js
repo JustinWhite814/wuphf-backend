6b93b4010fbe0c787dd2a0e9f50c6026d6bfd38e
rsaleh <raihan.saleh@gmail.com>
2021-05-17 09:13:56 -0400

    post router updates

--- controllers/postRouter.js
+++ controllers/postRouter.js
@@ -6,19 +6,22 @@ const Post = require("../models/PostModel");
 router.get("/", (req, res, next) => {
   Post.find({})
     // .populate('author')
-    // .exec((error, authors) => {
+    // .exec((error, posts) => {
     //   if (error) return console.log('author population error')
+    //   console.log(JSON.stringify(posts, null, "\t"))
     // })
     .then((posts) => res.json(posts))
     .catch(next);
 });
 
 // Show: Filter Posts by username
+// username will change to userId, we should switch to a try, catch approach for this
 router.get("/:username", (req, res, next) => {
   Post.find({author: req.params.username})
-    // .populate("author")
-    // .exec((error, author) => {
-    //   if (error) return console.log("author population error");
+    // .populate('author')
+    // .exec((error, posts) => {
+    //   if (error) return console.log('author population error')
+    //   console.log(JSON.stringify(posts, null, "\t"))
     // })
     .then((posts) => res.json(posts))
     .catch(next);
@@ -27,9 +30,10 @@ router.get("/:username", (req, res, next) => {
 // Show: Get a Post by ID
 router.get("/:id", (req, res, next) => {
   Post.findById(req.params.id)
-    // .populate("author")
-    // .exec((error, author) => {
-    //   if (error) return console.log("author population error");
+    // .populate('author')
+    // .exec((error, posts) => {
+    //   if (error) return console.log('author population error')
+    //   console.log(JSON.stringify(posts, null, "\t"))
     // })
     .then((posts) => res.json(posts))
     .catch(next);
